6e4bb855fdf64f8d6927872aa57cce7ac9172fad
Milan Chrenko <milan.chrenko@gamil.com>
2021-02-28 11:06:51 +0100

    fixe first

--- examples/1-hello/app.js
+++ examples/1-hello/app.js
@@ -3,11 +3,11 @@ const fetchData = async() =>{
     try {
         
 
-        //const {data} = await axios.get('/.netlify/functions/1-hello');
-        const {data} = await axios.get('/api/1-hello');
+        const {data} = await axios.get('/.netlify/functions/1-hello');
+        //const {data} = await axios.get('/api/1-hello');
         result.textContent = data;
     } catch (error) {
-        console.log(error.response);
+        //console.log(error.response);
         result.textContent = error.response.data;
     }
 }
